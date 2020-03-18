e23e22b730629e622c70853ad586506d49df519b
Theepan Kanthavel <theepan@cutedrops.com>
2020-03-18 18:02:17 -0400

    more refactor

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
   "net/http"
 )
 
-func Validate(next http.Handler) http.Handler {
+func Handle(manipulatorFn func([][]string) string) http.Handler {
   return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
     if r.Method != http.MethodPost {
       w.WriteHeader(http.StatusMethodNotAllowed)
@@ -25,29 +25,17 @@ func Validate(next http.Handler) http.Handler {
       return
     }
     defer file.Close()
-    next.ServeHTTP(w, r)
+    records, err := csv.NewReader(file).ReadAll()
+    w.Write([]byte(manipulatorFn(records)))
   })
 }
 
 func main() {
-  handler := func(fn func([][]string) string) http.Handler {
-    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-      file, _, err := r.FormFile("file")
-      defer file.Close()
-      if err != nil {
-        w.Write([]byte(fmt.Sprintf("error %s", err.Error())))
-        return
-      }
-      records, err := csv.NewReader(file).ReadAll()
-      w.Write([]byte(fn(records)))
-    })
-  }
-
-  http.Handle("/echo", Validate(handler(Backend_Challenge.Echo)))
-  http.Handle("/invert", Validate(handler(Backend_Challenge.Invert)))
-  http.Handle("/flatten", Validate(handler(Backend_Challenge.Flatten)))
-  http.Handle("/sum", Validate(handler(Backend_Challenge.Sum)))
-  http.Handle("/multiply", Validate(handler(Backend_Challenge.Multiply)))
+  http.Handle("/echo", Handle(Backend_Challenge.Echo))
+  http.Handle("/invert", Handle(Backend_Challenge.Invert))
+  http.Handle("/flatten", Handle(Backend_Challenge.Flatten))
+  http.Handle("/sum", Handle(Backend_Challenge.Sum))
+  http.Handle("/multiply", Handle(Backend_Challenge.Multiply))
   log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
